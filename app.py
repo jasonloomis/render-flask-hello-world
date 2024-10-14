d38f665c98c3df3a083c5bab7729bea5abc10a6e
Jason Loomis <18121229+jasonloomis@users.noreply.github.com>
2024-10-13 17:41:22 -0700

    Test request Yoto url.

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import urllib.request
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/yoto')
+def yoto():
+    yotoUrl = 'https://yoto.io/eb9jP?84jbYiDmy1gr=3qWnGm7RLt1CI' # crackling fire
+    html = urllib.request.urlopen(yotoUrl)
+    return html
